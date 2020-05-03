2b5d108e5254a38af6bd8bb15ad2ebd4689f72e4
Edin Omeragic <edin.omeragic@gmail.com>
2020-05-03 17:19:36 +0200

    Updated Vb.Net example.

--- vb/RayTracer.vb
+++ vb/RayTracer.vb
@@ -1,23 +1,21 @@
-﻿Imports VBRayTracer
+﻿Imports System.IO
+Imports System.Runtime.InteropServices
 
 Module RayTracer
+
     Sub Main()
-        Dim bmp As Drawing.Bitmap = New Drawing.Bitmap(500, 500, Drawing.Imaging.PixelFormat.Format32bppArgb)
         Dim sw As New Stopwatch()
-
         Console.WriteLine("VB.Net RayTracer Test")
-
         sw.Start()
-        Dim rayTracer As New RayTracerEngine()
+        Dim image As New Image(500, 500)
         Dim scene As New Scene()
-        rayTracer.Render(scene, bmp)
+        Dim rayTracer As New RayTracerEngine()
+        rayTracer.Render(scene, image)
         sw.Stop()
-
-        bmp.Save("vb-ray-tracer.png")
-
-        Console.WriteLine("")
+        image.Save("vb-ray.png")
         Console.WriteLine("Total time: " + sw.ElapsedMilliseconds.ToString() + " ms")
     End Sub
+
 End Module
 
 Structure Vector
@@ -62,7 +60,102 @@ Structure Vector
                           Z * v.X - X * v.Z,
                           X * v.Y - Y * v.X)
     End Function
+
 End Structure
+
+Structure RGBColor
+    Public B As Byte
+    Public G As Byte
+    Public R As Byte
+    Public A As Byte
+End Structure
+
+<StructLayout(LayoutKind.Sequential, Pack:=1)>
+Structure BITMAPINFOHEADER
+    Public biSize As UInt32
+    Public biWidth As Int32
+    Public biHeight As Int32
+    Public biPlanes As Int16
+    Public biBitCount As Int16
+    Public biCompression As UInt32
+    Public biSizeImage As UInt32
+    Public biXPelsPerMeter As Int32
+    Public biYPelsPerMeter As Int32
+    Public biClrUsed As UInt32
+    Public biClrImportant As UInt32
+End Structure
+
+<StructLayout(LayoutKind.Sequential, Pack:=1)>
+Structure BITMAPFILEHEADER
+    Public bfType As UInt16
+    Public bfSize As UInt32
+    Public bfOffBits As UInt32
+    Public bfReserved1 As UInt16
+    Public bfReserved2 As UInt16
+End Structure
+
+Class Image
+    Public ReadOnly Width As Integer
+    Public ReadOnly Height As Integer
+    Private Data As RGBColor()
+
+    Public Sub New(w As Integer, h As Integer)
+        Width = w
+        Height = h
+        Data = New RGBColor(w * h - 1) {}
+    End Sub
+
+    Public Sub SetColor(x As Integer, y As Integer, c As RGBColor)
+        Data(y * Height + x) = c
+    End Sub
+
+    Public Sub Save(fileName As String)
+
+        Dim infoHeaderSize = Marshal.SizeOf(GetType(BITMAPINFOHEADER))
+        Dim fileHeaderSize = Marshal.SizeOf(GetType(BITMAPFILEHEADER))
+        Dim offBits = infoHeaderSize + fileHeaderSize
+
+        Dim infoHeader = New BITMAPINFOHEADER With {
+            .biSize = CUInt(infoHeaderSize),
+            .biBitCount = 32,
+            .biClrImportant = 0,
+            .biClrUsed = 0,
+            .biCompression = 0,
+            .biHeight = -Height,
+            .biWidth = Width,
+            .biPlanes = 1,
+            .biSizeImage = CUInt((Width * Height * 4))
+        }
+
+        Dim fileHeader = New BITMAPFILEHEADER With {
+            .bfType = 66 + (77 << 8),
+            .bfOffBits = CUInt(offBits),
+            .bfSize = CUInt(offBits + infoHeader.biSizeImage)
+        }
+
+        Using writer = New BinaryWriter(System.IO.File.Open(fileName, FileMode.Create))
+            writer.Write(GetBytes(fileHeader))
+            writer.Write(GetBytes(infoHeader))
+            For Each color In Data
+                writer.Write(color.B)
+                writer.Write(color.G)
+                writer.Write(color.R)
+                writer.Write(color.A)
+            Next
+        End Using
+    End Sub
+
+    Private Function GetBytes(Of T)(data As T) As Byte()
+        Dim length = Marshal.SizeOf(data)
+        Dim ptr = Marshal.AllocHGlobal(length)
+        Dim result = New Byte(length - 1) {}
+        Marshal.StructureToPtr(data, ptr, True)
+        Marshal.Copy(ptr, result, 0, length)
+        Marshal.FreeHGlobal(ptr)
+        Return result
+    End Function
+
+End Class
 
 Structure Color
     Public R As Double
@@ -93,16 +186,16 @@ Structure Color
         Return New Color(v1.R * v2.R, v1.G * v2.G, v1.B * v2.B)
     End Operator
 
-    Public Function ToDrawingColor() As System.Drawing.Color
-        Return System.Drawing.Color.FromArgb(Clamp(R), Clamp(G), Clamp(B))
+    Public Function ToDrawingColor() As RGBColor
+        Return New RGBColor With {.R = Clamp(R), .G = Clamp(G), .B = Clamp(B), .A = 255}
     End Function
 
     Public Shared Function Clamp(c As Double) As Byte
-        Dim v As Integer = CInt(c * 255)
-        If (v > 255) Then Return 255
-        If (v < 0) Then Return 0
-        Return CType(v, Byte)
+        If (c > 1.0) Then Return 255
+        If (c < 0.0) Then Return 0
+        Return CByte(c * 255)
     End Function
+
 End Structure
 
 Class Camera
@@ -111,14 +204,20 @@ Class Camera
     Public Up As Vector
     Public Pos As Vector
 
-    Public Sub New(pos As Vector, lookAt As Vector)
+    Public Sub New(position As Vector, lookAt As Vector)
         Dim down = New Vector(0.0, -1.0, 0.0)
-        Me.Pos = pos
-
-        Forward = (lookAt - Me.Pos).Norm
+        Pos = position
+        Forward = (lookAt - position).Norm
         Right = 1.5 * Forward.Cross(down).Norm
         Up = 1.5 * Forward.Cross(Right).Norm
     End Sub
+
+    Function GetPoint(x As Integer, y As Integer, w As Integer, h As Integer) As Vector
+        Dim recenterX = (x - (w / 2.0)) / 2.0 / w
+        Dim recenterY = -(y - (h / 2.0)) / 2.0 / h
+        Return (Me.Forward + (recenterX * Me.Right) + (recenterY * Me.Up)).Norm()
+    End Function
+
 End Class
 
 Structure Ray
@@ -129,6 +228,7 @@ Structure Ray
         Me.Start = start
         Me.Dir = dir
     End Sub
+
 End Structure
 
 Class Intersection
@@ -141,6 +241,7 @@ Class Intersection
         Me.Ray = ray
         Me.Dist = dist
     End Sub
+
 End Class
 
 Structure SurfaceProperties
@@ -148,15 +249,28 @@ Structure SurfaceProperties
     Public Specular As Color
     Public Reflect As Double
     Public Roughness As Double
+
+    Public Sub New(diffuse As Color, specular As Color, reflect As Double, roughness As Double)
+        Me.Diffuse = diffuse
+        Me.Specular = specular
+        Me.Reflect = reflect
+        Me.Roughness = roughness
+    End Sub
+
 End Structure
 
 Interface ISurface
+
     Function GetSurfaceProperties(ByRef pos As Vector) As SurfaceProperties
+
 End Interface
 
 Interface IThing
+
     Function Intersect(ByRef ray As Ray) As Intersection
+
     Function Normal(ByRef pos As Vector) As Vector
+
     Property Surface As ISurface
 End Interface
 
@@ -168,12 +282,13 @@ Class Light
         Me.Pos = pos
         Me.Color = color
     End Sub
+
 End Class
 
 Class Sphere
     Implements IThing
 
-    Private Radius2 As Double
+    Private ReadOnly Radius2 As Double
     Private Center As Vector
 
     Public Sub New(center As Vector, radius As Double, surface As ISurface)
@@ -206,7 +321,7 @@ Class Plane
     Implements IThing
 
     Private Normal As Vector
-    Private Offset As Double
+    Private ReadOnly Offset As Double
 
     Public Sub New(normal As Vector, offset As Double, surface As ISurface)
         Me.Normal = normal
@@ -216,10 +331,11 @@ Class Plane
 
     Public Function Intersect(ByRef ray As Ray) As Intersection Implements IThing.Intersect
         Dim denom = Me.Normal.Dot(ray.Dir)
-        If (denom > 0) Then Return Nothing
-
-        Dim dist = (Me.Normal.Dot(ray.Start) + Me.Offset) / (-denom)
-        Return New Intersection(Me, ray, dist)
+        If (denom <= 0) Then
+            Dim dist = (Me.Normal.Dot(ray.Start) + Me.Offset) / (-denom)
+            Return New Intersection(Me, ray, dist)
+        End If
+        Return Nothing
     End Function
 
     Public Function GetNormal(ByRef pos As Vector) As Vector Implements IThing.Normal
@@ -233,39 +349,24 @@ Class ShinySurface
     Implements ISurface
 
     Public Function GetSurfaceProperties(ByRef pos As Vector) As SurfaceProperties Implements ISurface.GetSurfaceProperties
-        Return New SurfaceProperties With {
-            .Specular = Color.Grey,
-            .Diffuse = Color.White,
-            .Reflect = 0.7,
-            .Roughness = 250
-        }
+        Return New SurfaceProperties(Color.White, Color.Grey, 0.7, 250)
     End Function
+
 End Class
 
 Class CheckerboardSurface
     Implements ISurface
 
     Public Function GetSurfaceProperties(ByRef pos As Vector) As SurfaceProperties Implements ISurface.GetSurfaceProperties
-        Dim diffuse = Color.Black
-        Dim reflect = 0.7
+        Dim condition = (Math.Floor(pos.Z) + Math.Floor(pos.X)) Mod 2 <> 0
 
-        If (Math.Floor(pos.Z) + Math.Floor(pos.X)) Mod 2 <> 0 Then
-            diffuse = Color.White
-            reflect = 0.1
+        If condition Then
+            Return New SurfaceProperties(Color.White, Color.White, 0.1, 150)
         End If
 
-        Return New SurfaceProperties With {
-            .Specular = Color.White,
-            .Diffuse = diffuse,
-            .Reflect = reflect,
-            .Roughness = 150
-        }
+        Return New SurfaceProperties(Color.Black, Color.White, 0.7, 150)
     End Function
-End Class
 
-Class Surfaces
-    Public Shared Shiny As ISurface = New ShinySurface()
-    Public Shared CheckerBoard As ISurface = New CheckerboardSurface()
 End Class
 
 Class Scene
@@ -274,21 +375,26 @@ Class Scene
     Public Things As List(Of IThing) = New List(Of IThing)
 
     Public Sub New()
+
+        Dim Shiny As ISurface = New ShinySurface()
+        Dim CheckerBoard As ISurface = New CheckerboardSurface()
+
         Camera = New Camera(New Vector(3.0, 2.0, 4.0), New Vector(-1.0, 0.5, 0.0))
 
-        Things.Add(New Plane(New Vector(0.0, 1.0, 0.0), 0.0, Surfaces.CheckerBoard))
-        Things.Add(New Sphere(New Vector(0.0, 1.0, -0.25), 1.0, Surfaces.Shiny))
-        Things.Add(New Sphere(New Vector(-1.0, 0.5, 1.5), 0.5, Surfaces.Shiny))
+        Things.Add(New Plane(New Vector(0.0, 1.0, 0.0), 0.0, CheckerBoard))
+        Things.Add(New Sphere(New Vector(0.0, 1.0, -0.25), 1.0, Shiny))
+        Things.Add(New Sphere(New Vector(-1.0, 0.5, 1.5), 0.5, Shiny))
 
         Lights.Add(New Light(New Vector(-2.0, 2.5, 0.0), New Color(0.49, 0.07, 0.07)))
         Lights.Add(New Light(New Vector(1.5, 2.5, 1.5), New Color(0.07, 0.07, 0.49)))
         Lights.Add(New Light(New Vector(1.5, 2.5, -1.5), New Color(0.07, 0.49, 0.071)))
         Lights.Add(New Light(New Vector(0.0, 3.5, 0.0), New Color(0.21, 0.21, 0.35)))
     End Sub
+
 End Class
 
 Class RayTracerEngine
-    Private maxDepth As Integer = 5
+    Private ReadOnly MaxDepth As Integer = 5
     Private Scene As Scene
 
     Private Function Intersections(ByRef ray As Ray) As Intersection
@@ -302,16 +408,8 @@ Class RayTracerEngine
                 closest = inter.Dist
             End If
         Next
+
         Return closestInter
-    End Function
-
-    Private Function TestRay(ByRef ray As Ray) As Double?
-        Dim isect As Intersection = Me.Intersections(ray)
-        If (isect IsNot Nothing) Then
-            Return isect.Dist
-        End If
-
-        Return Nothing
     End Function
 
     Private Function TraceRay(ByRef ray As Ray, depth As Integer) As Color
@@ -331,7 +429,7 @@ Class RayTracerEngine
         Dim surface = isect.Thing.Surface.GetSurfaceProperties(pos)
 
         Dim naturalColor = Color.Background + Me.GetNaturalColor(surface, pos, normal, reflectDir)
-        Dim reflectedColor = If(depth >= Me.maxDepth, Color.Grey, Me.GetReflectionColor(surface, pos, reflectDir, depth))
+        Dim reflectedColor = If(depth >= Me.MaxDepth, Color.Grey, Me.GetReflectionColor(surface, pos, reflectDir, depth))
 
         Return naturalColor + reflectedColor
     End Function
@@ -341,66 +439,43 @@ Class RayTracerEngine
     End Function
 
     Private Function GetNaturalColor(surface As SurfaceProperties, pos As Vector, norm As Vector, rd As Vector) As Color
-
         Dim resultColor As Color = Color.DefaultColor
         Dim reflectDir = rd.Norm
 
         For i = 0 To Scene.Lights.Count - 1
             Dim light = Scene.Lights(i)
             Dim ldis = light.Pos - pos
-            Dim livec = ldis.Norm
-            Dim neatIsect = Me.TestRay(New Ray(pos, livec))
+            Dim livec = ldis.Norm()
+            Dim neatIsect = Me.Intersections(New Ray(pos, livec))
+            Dim isInShadow = If(neatIsect IsNot Nothing, (neatIsect.Dist <= ldis.Length), False)
 
-            Dim isInShadow = If(neatIsect.HasValue, (neatIsect <= ldis.Length), False)
             If (Not isInShadow) Then
-
                 Dim illum = livec.Dot(norm)
-                Dim lcolor = If((illum > 0), illum * light.Color, Color.DefaultColor)
-
                 Dim specular = livec.Dot(reflectDir)
+
+                Dim lcolor = If((illum > 0), illum * light.Color, Color.DefaultColor)
                 Dim scolor = If(specular > 0, (Math.Pow(specular, surface.Roughness) * light.Color), Color.DefaultColor)
 
                 resultColor = resultColor + (surface.Diffuse * lcolor) + (surface.Specular * scolor)
             End If
         Next
         Return resultColor
-
     End Function
 
-    Public Sub Render(scene As Scene, bmp As System.Drawing.Bitmap)
-        Dim w As Integer = bmp.Width
-        Dim h As Integer = bmp.Height
+    Public Sub Render(scene As Scene, image As Image)
         Me.Scene = scene
 
-        Dim GetPoint = Function(x As Integer, y As Integer, Camera As Camera) As Vector
-                           Dim recenterX = (x - (w / 2.0)) / 2.0 / w
-                           Dim recenterY = -(y - (h / 2.0)) / 2.0 / h
-                           Return (Camera.Forward + (recenterX * Camera.Right) + (recenterY * Camera.Up)).Norm
-                       End Function
-
-        Dim bitmapData As Drawing.Imaging.BitmapData = bmp.LockBits(New Drawing.Rectangle(0, 0, w, h), Drawing.Imaging.ImageLockMode.ReadWrite, bmp.PixelFormat)
-        Dim stride As Integer = bitmapData.Stride
-        Dim rgbData() As Byte
-        Dim size As Integer = stride * h
-        ReDim rgbData(size)
-
-        System.Runtime.InteropServices.Marshal.Copy(bitmapData.Scan0, rgbData, 0, size)
-
-        Dim ray = New Ray(scene.Camera.Pos, New Vector(0, 0, 0))
+        Dim w As Integer = image.Width
+        Dim h As Integer = image.Height
 
         For y = 0 To h - 1
             For x = 0 To w - 1
-                ray.Dir = GetPoint(x, y, scene.Camera)
-                Dim color = Me.TraceRay(ray, 0).ToDrawingColor()
-                Dim pos = y * stride + x * 4
-                rgbData(pos + 0) = color.B
-                rgbData(pos + 1) = color.G
-                rgbData(pos + 2) = color.R
-                rgbData(pos + 3) = 255
+                Dim point = scene.Camera.GetPoint(x, y, w, h)
+                Dim ray = New Ray(scene.Camera.Pos, point)
+                Dim color = TraceRay(ray, 0).ToDrawingColor()
+                image.SetColor(x, y, color)
             Next
         Next
-
-        System.Runtime.InteropServices.Marshal.Copy(rgbData, 0, bitmapData.Scan0, size)
-        bmp.UnlockBits(bitmapData)
     End Sub
+
 End Class
